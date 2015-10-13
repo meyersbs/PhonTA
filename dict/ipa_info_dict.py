7f2679044467b0ede9f12d245bb65776b8b31560
Benjamin Meyers <meyersbs@users.noreply.github.com>
2015-10-13 18:12:44 -0400

    Update ipa_info_dict.py

--- dict/ipa_info_dict.py
+++ dict/ipa_info_dict.py
@@ -1,18 +1,19 @@
-ipa = { 000:    ["Unicode",         "Decimal",          "Voicing",      "Place of Articulation",    "Manner of Articulation"],
-        101:    ["U+0070",          "&#112;",           "voiced",       "bilabial",                 "plosive,stop"], # p
-        101408: ["U+0070 U+032A",   "&#112;&#810;",     "voiceless",    "labiodental",              "plosive,stop"],
-        102:    ["U+0062",          "&#98;",            "voiceless",    "bilabial",                 "plosive,stop"], # b
-        102408: ["U+0062 U+032A",   "&#98;&#810;",      "voiced",       "labiodental",              "plosive,stop"],
-        103:    ["U+0074",          "&#116;",           "voiceless",    "alveolar",                 "plosive,stop"], # t
-        104:    ["U+0064",          "&#100;",           "voiced",       "alveolar",                 "plosive,stop"], # d
-        105:    ["U+0288",          "&#648;",           "voiceless",    "retroflex",                "plosive,stop"],
-        106:    ["U+0256",          "&#598;",           "voiced",       "retroflex",                "plosive,stop"],
-        107:    ["U+0063",          "&#99;",            "voiceless",    "palatal",                  "plosive,stop"], # c
-        108:    ["U+025F",          "&#607;",           "voiced",       "palatal",                  "plosive,stop"],
-        109:    ["U+006B",          "&#107;",           "voiceless",    "velar",                    "plosive,stop"], # k
-        110:    ["U+0261",          "&#609;",           "voiced",       "velar",                    "plosive,stop"], # g
-        111:    ["U+0071",          "&#113;",           "voiceless",    "uvular",                   "plosive,stop"], # q
-        112:    ["U+0262",          "&#610;",           "voiced",       "uvular",                   "plosive,stop"], # G
-        113:    ["U+0294",          "&#660;",           "N/A",          "glottal",                  "plosive,stop"],
-        173:    ["U+02A1",          "&#673;",           "N/A",          "epiglottal,pharyngeal",    "plosive,stop"]
-        }
+ipa = {
+000:    ["Unicode",         "Decimal",          "Voicing",      "Place of Articulation",    "Manner of Articulation"],
+101:    ["U+0070",          "&#112;",           "voiced",       "bilabial",                 "plosive,stop"], # p
+101408: ["U+0070 U+032A",   "&#112;&#810;",     "voiceless",    "labiodental",              "plosive,stop"],
+102:    ["U+0062",          "&#98;",            "voiceless",    "bilabial",                 "plosive,stop"], # b
+102408: ["U+0062 U+032A",   "&#98;&#810;",      "voiced",       "labiodental",              "plosive,stop"],
+103:    ["U+0074",          "&#116;",           "voiceless",    "alveolar",                 "plosive,stop"], # t
+104:    ["U+0064",          "&#100;",           "voiced",       "alveolar",                 "plosive,stop"], # d
+105:    ["U+0288",          "&#648;",           "voiceless",    "retroflex",                "plosive,stop"],
+106:    ["U+0256",          "&#598;",           "voiced",       "retroflex",                "plosive,stop"],
+107:    ["U+0063",          "&#99;",            "voiceless",    "palatal",                  "plosive,stop"], # c
+108:    ["U+025F",          "&#607;",           "voiced",       "palatal",                  "plosive,stop"],
+109:    ["U+006B",          "&#107;",           "voiceless",    "velar",                    "plosive,stop"], # k
+110:    ["U+0261",          "&#609;",           "voiced",       "velar",                    "plosive,stop"], # g
+111:    ["U+0071",          "&#113;",           "voiceless",    "uvular",                   "plosive,stop"], # q
+112:    ["U+0262",          "&#610;",           "voiced",       "uvular",                   "plosive,stop"], # G
+113:    ["U+0294",          "&#660;",           "N/A",          "glottal",                  "plosive,stop"],
+173:    ["U+02A1",          "&#673;",           "N/A",          "epiglottal,pharyngeal",    "plosive,stop"]
+}
