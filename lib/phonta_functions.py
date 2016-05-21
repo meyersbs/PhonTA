09f281998bf2e767ff335633a05a353ad1a4dec8
meyersbs <meyers.bs@gmail.com>
2016-05-21 12:32:24 -0400

    Dictionary sorting using BASH.

--- lib/phonta_functions.py
+++ lib/phonta_functions.py
@@ -1,2 +1,22 @@
 
 #!/usr/bin/python
+
+#################################################################################
+# @PROJECT: PhonTA - Phonetic Transcription Assistant				#
+# @VERSION:									#
+# @AUTHOR: Benjamin Meyers							#
+# @EMAIL: ben@lionlogic.org							#
+# @LICENSE: MIT									#
+#################################################################################
+
+import os, subprocess
+from shutil import copyfile
+
+def sort_dict(dictionary):
+	""" Takes the given dictionary and sorts it alphabetically. """
+	if os.path.exists(dictionary):
+		subprocess.call(['sort', '-V', dictionary, '-o', dictionary], stdout=None)
+		return True
+	else:
+		return False
+
