a8bbefd091336167f75852d982df190a8586801d
Benjamin Meyers <ben@lionlogic.org>
2016-05-31 19:31:14 -0400

    Translation mostly works. Dictionary updating mostly works.

--- dict/util.py
+++ dict/util.py
@@ -1,6 +1,3 @@
-
-#!/usr/bin/python
-
 #################################################################################
 # @PROJECT: PhonTA - Phonetic Transcription Assistant				#
 # @VERSION:									#
@@ -13,6 +10,8 @@
 import os, subprocess, re
 
 ##### VARIABLES #################################################################
+curr_dir = os.path.dirname(__file__)
+default_dictionary = curr_dir + "/english"
 arpa_to_ipa = {	"AA":u'\u0251', "AA0":u'\u0251', "AA1":u'\u0251', "AA2":u'\u0251',
 		"AE":u'\u00E6', "AE0":u'\u00E6', "AE1":u'\u00E6', "AE2":u'\u00E6',
 		"AH":u'\u028C', "AH0":u'\u028C', "AH1":u'\u028C', "AH2":u'\u028C',
@@ -48,43 +47,49 @@ arpa_to_ipa = {	"AA":u'\u0251', "AA0":u'\u0251', "AA1":u'\u0251', "AA2":u'\u0251
 		"Y":u'\u0079',
 		"Z":u'\u007A',  "ZH":u'\u0292'}
 
-def format_cmudict(dictionary):
+def download_cmudict():
+	print("Downloading cmudict...")
+	subprocess.call(['wget', 'http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b', '-P', curr_dir], stdout=None)
+	temp_dict = curr_dir + '/cmudict-0.7b'
+	subprocess.call(['cp', temp_dict, default_dictionary], stdout=None)
+	subprocess.call(['rm', temp_dict], stdout=None)
+
+def format_cmudict():
 	""" Takes the given cmudict file and reformats it to CSV. """
 	new_dict = open("dictionary.psv", "a")
 	print("Formatting cmudict...")
-	if os.path.exists(dictionary):
-		with open(dictionary, "r") as f:
+	if os.path.exists(default_dictionary):
+		with open(default_dictionary, "r") as f:
 			for line in f:
 				if ";;;" not in line and re.match(r'[A-Za-z\']', line[0]):
 					temp_line = re.split(r'\s\s', line)
 					new_dict.write(temp_line[0] + "|" + temp_line[1].strip("\n") + "\n")
 
-		# Add missing terms
+		# Add missing terms.
 		new_dict.write("&|AE2 N D\n")
 		new_dict.write("@|AE2 T\n")
+		# Close the file and cleanup the directory.
 		new_dict.close()
-		subprocess.call(['cp', "dictionary.psv", dictionary], stdout=None)
+		subprocess.call(['cp', "dictionary.psv", default_dictionary], stdout=None)
 		subprocess.call(['rm', "dictionary.psv"], stdout=None)
 		return True
 	return False
 
-def sort_cmudict(dictionary):
+def sort_cmudict():
 	""" Takes the given dictionary and sorts it alphabetically. """
 	print("Sorting cmudict...")
-	if os.path.exists(dictionary):
-		subprocess.call(['sort', '-V', dictionary, '-o', dictionary], stdout=None)
+	if os.path.exists(default_dictionary):
+		subprocess.call(['sort', '-V', default_dictionary, '-o', default_dictionary], stdout=None)
 		return True
 	else:
 		return False
 
-def update_cmudict(dictionary):
+def update_cmudict():
 	""" Converts the ARPABET given in the cmudict into IPA. """
 	print("Updating cmudict...")
-	format_cmudict(dictionary)
-	sort_cmudict(dictionary)
 	new_dict = open("dictionary.psv", "wb")
-	if os.path.exists(dictionary):
-		with open(dictionary, "r") as f:
+	if os.path.exists(default_dictionary):
+		with open(default_dictionary, "r") as f:
 			for line in f:
 				elems = line.split("|")
 				temp_line = ""
@@ -96,10 +101,51 @@ def update_cmudict(dictionary):
 						temp_line += arpa_to_ipa[item] + " "
 				new_dict.write(line.strip("\n") + "|" + temp_line.encode('utf8').rstrip() + "\n")
 		new_dict.close()
-                subprocess.call(['cp', "dictionary.psv", dictionary], stdout=None)
+                subprocess.call(['cp', "dictionary.psv", default_dictionary], stdout=None)
                 subprocess.call(['rm', "dictionary.psv"], stdout=None)
 		return True
 	return False
 
+def upgrade_cmudict():
+	download_cmudict()
+	format_cmudict()
+	sort_cmudict()
+	update_cmudict()
+	return "Successfully upgraded to the newest cmudict!"
+
+def search_cmudict(term, encoding="IPA"):
+        with open(default_dictionary, "r") as f:
+                for line in f:
+                        temp_line = line.split("|")
+                        if temp_line[0] == term.upper().strip(".").strip("!").strip("?"):
+                                if encoding == "IPA":
+                                        return temp_line[2].rstrip().strip("\n")
+                                else:
+                                        return temp_line[1].rstrip().strip("\n")
+        return "ERROR"
+
+def add_to_cmudict(term, arpabet, ipa="NONE"):
+	if search_cmudict(term) != "ERROR":
+		return "Already in cmudict."
+	else:
+		if ipa == "NONE":
+			ipa = search_cmudict(term, "IPA")
+		else:
+			pass
+		
+		with open(default_dictionary, "ab") as f:
+			f.write(term + "|" + arpabet + "|" + ipa + "\n")
+
+		sort_cmudict()
+	return "Successfully added '" + term + "' to the cmudict."		
+
 def translate(text):
-	return False
+	arpa = ""
+	ipa = ""
+	for term in text.split():
+		arpa += search_cmudict(term, "ARPABET") + "   "
+		ipa += search_cmudict(term, "IPA") + "   "
+	print("ARPABET:\t" + arpa)
+	print("IPA:\t\t" + ipa)
+	return ""
+
